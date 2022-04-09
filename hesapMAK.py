a860cbbf14e9c05360c998658ef6eefcbe611866
Elif Gider <giderelenes@gmail.com>
2022-04-09 17:08:32 +0300

    hata uyarısı verildi.

--- hesapMAK.py
+++ hesapMAK.py
@@ -5,3 +5,9 @@ print("Toplam:",toplam)
 
 bolme=sayi1/sayi2
 print("Hatalı bölme:",bolme)
+
+try:
+    bolum = sayi1/sayi2
+    print("Bölüm:",bolum)
+except ZeroDivisionError:
+    print("Bir sayıyı sıfıra bölemezsiniz.")
